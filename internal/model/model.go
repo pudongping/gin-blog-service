3a53f6b5f69204b3ea030477e1ab26ccb59099ea
pudongping <276558492@qq.com>
2022-01-07 11:55:07 +0800

    初始化数据库连接

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,5 +1,19 @@
 package model
 
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+
+	// GORM 的 MySQL 数据库驱动导入
+	"gorm.io/driver/mysql"
+
+	"github.com/pudongping/gin-blog-service/global"
+	"github.com/pudongping/gin-blog-service/pkg/setting"
+)
+
 type Model struct {
 	// id  int(10) unsigned is_nullable NO
 	Id uint32 `gorm:"primary_key" json:"id"`
@@ -15,4 +29,61 @@ type Model struct {
 	DeletedOn uint32 `json:"deleted_on"`
 	// 是否删除 0 为未删除、1 为已删除  tinyint(3) unsigned is_nullable YES
 	IsDel uint8 `json:"is_del"`
+}
+
+func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
+
+	// user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local",
+		databaseSetting.UserName,
+		databaseSetting.Password,
+		databaseSetting.Host,
+		databaseSetting.DBName,
+		databaseSetting.Charset,
+		databaseSetting.ParseTime,
+	)
+
+	config := mysql.New(mysql.Config{
+		DSN: dsn,
+	})
+
+	var level logger.LogLevel
+	// 允许我们在命令行里查看请求的 sql 信息
+	// Silent —— 静默模式，不打印任何信息
+	// Error —— 发生错误了才打印
+	// Warn —— 发生警告级别以上的错误才打印
+	// Info —— 打印所有信息，包括 SQL 语句
+	if global.ServerSetting.RunMode == "debug" {
+		level = logger.Info
+	} else {
+		level = logger.Error
+	}
+
+	db, err := gorm.Open(config, &gorm.Config{
+		Logger: logger.Default.LogMode(level),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// 命令行打印数据库请求的信息
+	// *gorm.DB 对象的 DB() 方法，可以直接获取到 database/sql 包里的 *sql.DB 对象
+	sqlDB, _ := db.DB()
+	// 设置最大连接数
+	sqlDB.SetMaxOpenConns(databaseSetting.MaxOpenConns)
+	// 设置最大空闲连接数
+	sqlDB.SetMaxIdleConns(databaseSetting.MaxIdleConns)
+	// 设置每个链接的过期时间
+	sqlDB.SetConnMaxLifetime(time.Duration(databaseSetting.ConnMaxLifetime) * time.Second)
+
+	// 创建和维护数据表结构
+	migration(db)
+
+	return db, nil
+}
+
+// migration 自动迁移
+func migration(db *gorm.DB) {
+	// db.AutoMigrate()
 }
--- main.go
+++ main.go
@@ -9,15 +9,24 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/pudongping/gin-blog-service/global"
+	"github.com/pudongping/gin-blog-service/internal/model"
 	"github.com/pudongping/gin-blog-service/internal/routers"
 	"github.com/pudongping/gin-blog-service/pkg/setting"
 )
 
 func init() {
+	// 初始化加载配置信息
 	err := setupSetting()
 	if err != nil {
 		log.Fatalf("init.setupSetting err: %v", err)
 	}
+
+	// 初始化连接数据库
+	err = setupDBEngine()
+	if err != nil {
+		log.Fatalf("init.setupDBEngine err: %v", err)
+	}
+
 }
 
 func main() {
@@ -43,16 +52,15 @@ func setupSetting() error {
 		return err
 	}
 
+	// 将读取到的配置信息绑定到对应的结构体中
 	err = newSetting.ReadSection("Server", &global.ServerSetting)
 	if err != nil {
 		return err
 	}
-
 	err = newSetting.ReadSection("App", &global.AppSetting)
 	if err != nil {
 		return err
 	}
-
 	err = newSetting.ReadSection("Database", &global.DatabaseSetting)
 	if err != nil {
 		return err
@@ -60,5 +68,19 @@ func setupSetting() error {
 
 	global.ServerSetting.ReadTimeout *= time.Second
 	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+}
+
+// setupDBEngine 初始化设置数据库连接
+func setupDBEngine() error {
+	var err error
+	// 这里需要注意：不能写成 ==> global.DBEngine, err := model.NewDBEngine(global.DatabaseSetting)
+	// 因为 `:=` 会重新声明并创建了左侧的新局部变量，因此在其它包中调用 global.DBEngine 变量时，它仍然是 nil
+	// 因为根本就没有赋值到包全局变量 global.DBEngine 上
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
