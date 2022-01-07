df07f3ce7838c07e174ee358a7044eb2c3ece304
pudongping <276558492@qq.com>
2022-01-07 09:42:47 +0800

    封装读取配置信息的组件

---
+++ global/setting.go
@@ -0,0 +1,11 @@
+package global
+
+import (
+	"github.com/pudongping/gin-blog-service/pkg/setting"
+)
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	DatabaseSetting *setting.DatabaseSettingS
+)
---
+++ pkg/setting/setting.go
@@ -0,0 +1,22 @@
+package setting
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Setting struct {
+	vp *viper.Viper
+}
+
+func NewSetting() (*Setting, error) {
+	vp := viper.New()
+	vp.SetConfigName("config")   // 设置配置文件的名称为 config
+	vp.AddConfigPath("configs/") // 设置其配置路径为相对路径 configs/
+	vp.SetConfigType("yaml")     // 配置类型为 yaml
+	err := vp.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Setting{vp: vp}, nil
+}
