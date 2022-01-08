9934eceb63ac09d2bd1a55c2cd9acada784a5d26
pudongping <276558492@qq.com>
2022-01-08 23:32:08 +0800

    定义 swagger 文档路由

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -2,6 +2,10 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+
+	_ "github.com/pudongping/gin-blog-service/docs"
 
 	v1 "github.com/pudongping/gin-blog-service/internal/routers/api/v1"
 )
@@ -10,6 +14,10 @@ func NewRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+
+	// swaggerUrl := ginSwagger.URL("http://127.0.0.1:8000/swagger/doc.json")
+	// r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, swaggerUrl))
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	article := v1.NewArticle()
 	tag := v1.NewTag()
