0ddf084ba41c55c44a1d9f5b4d50e66d5ad98d3f
pudongping <276558492@qq.com>
2022-01-10 01:37:33 +0800

    路由注释

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -26,18 +26,16 @@ func NewRouter() *gin.Engine {
 
 	apiv1 := r.Group("/api/v1")
 	{
-		apiv1.POST("/tags", tag.Create)
-		apiv1.DELETE("/tags/:id", tag.Delete)
-		apiv1.PUT("/tags/:id", tag.Update)
-		apiv1.PATCH("/tags/:id/state", tag.Update)
-		apiv1.GET("/tags", tag.List)
+		apiv1.POST("/tags", tag.Create)  // 创建标签
+		apiv1.DELETE("/tags/:id", tag.Delete)  // 删除指定标签
+		apiv1.PUT("/tags/:id", tag.Update)  // 更新指定标签
+		apiv1.GET("/tags", tag.List)  // 获取标签列表
 
-		apiv1.POST("/articles", article.Create)
-		apiv1.DELETE("/articles/:id", article.Delete)
-		apiv1.PUT("/articles/:id", article.Update)
-		apiv1.PATCH("/articles/:id/state", article.Update)
-		apiv1.GET("/articles/:id", article.Get)
-		apiv1.GET("/articles", article.List)
+		apiv1.POST("/articles", article.Create)  // 创建文章
+		apiv1.DELETE("/articles/:id", article.Delete)  // 删除指定文章
+		apiv1.PUT("/articles/:id", article.Update)  // 更新指定文章
+		apiv1.GET("/articles/:id", article.Get)  // 获取指定文章
+		apiv1.GET("/articles", article.List)  // 获取文章列表
 	}
 
 	return r
