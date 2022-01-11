dd7e3aaec295beb2c4e1f3bc12e32c67542e6165
pudongping <276558492@qq.com>
2022-01-11 23:52:02 +0800

    Update file.go

--- pkg/upload/file.go
+++ pkg/upload/file.go
@@ -1,6 +1,9 @@
 package upload
 
 import (
+	"io/ioutil"
+	"mime/multipart"
+	"os"
 	"path"
 	"strings"
 
@@ -33,4 +36,48 @@ func GetFileExt(name string) string {
 // 获取文件保存地址
 func GetSavePath() string {
 	return global.AppSetting.UploadSavePath
+}
+
+// 检查保存目录是否存在
+func CheckSavePath(dst string) bool {
+	_, err := os.Stat(dst) // 获取文件的描述信息 FileInfo
+	return os.IsNotExist(err)
+}
+
+// 检查文件后缀是否包含在约定的后缀配置项中
+func CheckContainExt(t FileType, name string) bool {
+	ext := GetFileExt(name)
+	ext = strings.ToUpper(ext)
+	switch t {
+	case TypeImage:
+		for _, allowExt := range global.AppSetting.UploadImageAllowExts {
+			if strings.ToUpper(allowExt) == ext {
+				return true
+			}
+		}
+
+	}
+
+	return false
+}
+
+// 检查文件大小是否超出最大大小限制
+func CheckMaxSize(t FileType, f multipart.File) bool {
+	content, _ := ioutil.ReadAll(f)
+	size := len(content)
+	switch t {
+	case TypeImage:
+		if size >= global.AppSetting.UploadImageMaxSize*1024*1024 {
+			return true
+		}
+
+	}
+
+	return false
+}
+
+// 检查文件权限是否足够
+func CheckPermission(dst string) bool {
+	_, err := os.Stat(dst)
+	return os.IsPermission(err)
 }
