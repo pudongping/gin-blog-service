d5cb7388b303f9e1796ba17bf50114933fa0c980
pudongping <276558492@qq.com>
2022-01-12 01:00:33 +0800

    图片上传路由

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/pudongping/gin-blog-service/internal/routers/api"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
 
@@ -23,19 +24,22 @@ func NewRouter() *gin.Engine {
 
 	article := v1.NewArticle()
 	tag := v1.NewTag()
+	upload := api.NewUpload()
+
+	r.POST("/upload/file", upload.UploadFile) // 上传文件接口
 
 	apiv1 := r.Group("/api/v1")
 	{
-		apiv1.POST("/tags", tag.Create)  // 创建标签
-		apiv1.DELETE("/tags/:id", tag.Delete)  // 删除指定标签
-		apiv1.PUT("/tags/:id", tag.Update)  // 更新指定标签
-		apiv1.GET("/tags", tag.List)  // 获取标签列表
+		apiv1.POST("/tags", tag.Create)       // 创建标签
+		apiv1.DELETE("/tags/:id", tag.Delete) // 删除指定标签
+		apiv1.PUT("/tags/:id", tag.Update)    // 更新指定标签
+		apiv1.GET("/tags", tag.List)          // 获取标签列表
 
-		apiv1.POST("/articles", article.Create)  // 创建文章
-		apiv1.DELETE("/articles/:id", article.Delete)  // 删除指定文章
-		apiv1.PUT("/articles/:id", article.Update)  // 更新指定文章
-		apiv1.GET("/articles/:id", article.Get)  // 获取指定文章
-		apiv1.GET("/articles", article.List)  // 获取文章列表
+		apiv1.POST("/articles", article.Create)       // 创建文章
+		apiv1.DELETE("/articles/:id", article.Delete) // 删除指定文章
+		apiv1.PUT("/articles/:id", article.Update)    // 更新指定文章
+		apiv1.GET("/articles/:id", article.Get)       // 获取指定文章
+		apiv1.GET("/articles", article.List)          // 获取文章列表
 	}
 
 	return r
