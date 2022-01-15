b7aa7a6704fdea6ebf25d1342ebe991e3cedb964
pudongping <276558492@qq.com>
2022-01-16 02:17:19 +0800

    针对路由方法的令牌桶控制

---
+++ pkg/limiter/method_limiter.go
@@ -0,0 +1,49 @@
+package limiter
+
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/juju/ratelimit"
+)
+
+type MethodLimiter struct {
+	*Limiter
+}
+
+func NewMethodLimiter() LimiterIface {
+	l := &Limiter{limiterBuckets: make(map[string]*ratelimit.Bucket)}
+	return MethodLimiter{
+		Limiter: l,
+	}
+}
+
+func (m MethodLimiter) Key(c *gin.Context) string {
+	uri := c.Request.RequestURI
+	index := strings.Index(uri, "?") // 切割出核心路由
+	if index == -1 {
+		return uri
+	}
+
+	return uri[:index]
+}
+
+func (m MethodLimiter) GetBucket(key string) (*ratelimit.Bucket, bool) {
+	bucket, ok := m.limiterBuckets[key]
+	return bucket, ok
+}
+
+func (m MethodLimiter) AddBuckets(rules ...LimiterBucketRule) LimiterIface {
+	for _, rule := range rules {
+		if _, ok := m.limiterBuckets[rule.Key]; !ok {
+			bucket := ratelimit.NewBucketWithQuantum(
+				rule.FillInterval,
+				rule.Capacity,
+				rule.Quantum,
+			)
+			m.limiterBuckets[rule.Key] = bucket
+		}
+	}
+
+	return m
+}
