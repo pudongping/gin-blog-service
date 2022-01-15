43e224996dd4798ee03b5fc3274fabce5053402a
pudongping <276558492@qq.com>
2022-01-16 00:09:36 +0800

    访问日志记录

--- internal/middleware/access_log.go
+++ internal/middleware/access_log.go
@@ -35,16 +35,16 @@ func AccessLog() gin.HandlerFunc {
 		endTime := time.Now().Unix()
 
 		fields := logger.Fields{
-			"request":  c.Request.PostForm.Encode(),
-			"response": bodyWriter.body.String(),
+			"request":  c.Request.PostForm.Encode(), // 当前的请求参数
+			"response": bodyWriter.body.String(),    // 当前的请求结果响应主体
 		}
 		s := "access log: method: %s, status_code: %d, " +
 			"begin_time: %d, end_time: %d"
 		global.Logger.WithFields(fields).Infof(c, s,
-			c.Request.Method,
-			bodyWriter.Status(),
-			beginTime,
-			endTime,
+			c.Request.Method,    // 当前的调用方法
+			bodyWriter.Status(), // 当前的响应结果状态码
+			beginTime,           // 调用方法的开始时间
+			endTime,             // 调用方法的结束时间
 		)
 
 	}
