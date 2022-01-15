760169ac9a5653ed131919bb3d10b28d0da294f7
pudongping <276558492@qq.com>
2022-01-16 03:12:43 +0800

    Update main.go

--- main.go
+++ main.go
@@ -88,6 +88,7 @@ func setupSetting() error {
 		return err
 	}
 
+	global.AppSetting.DefaultContextTimeout *= time.Second
 	global.ServerSetting.ReadTimeout *= time.Second
 	global.ServerSetting.WriteTimeout *= time.Second
 	global.JWTSetting.Expire *= time.Second
