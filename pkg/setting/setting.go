26461b4c8c947bcf7f4a73e8d547160a61b5f6c6
pudongping <276558492@qq.com>
2022-01-18 23:35:40 +0800

    Update setting.go

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -8,11 +8,18 @@ type Setting struct {
 	vp *viper.Viper
 }
 
-func NewSetting() (*Setting, error) {
+func NewSetting(configs ...string) (*Setting, error) {
 	vp := viper.New()
-	vp.SetConfigName("config")   // 设置配置文件的名称为 config
-	vp.AddConfigPath("configs/") // 设置其配置路径为相对路径 configs/
-	vp.SetConfigType("yaml")     // 配置类型为 yaml
+	vp.SetConfigName("config") // 设置配置文件的名称为 config
+
+	for _, config := range configs {
+		if config != "" {
+			vp.AddConfigPath(config)
+			// vp.AddConfigPath("configs/") // 设置其配置路径为相对路径 configs/
+		}
+	}
+
+	vp.SetConfigType("yaml") // 配置类型为 yaml
 	err := vp.ReadInConfig()
 	if err != nil {
 		return nil, err
